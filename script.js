7835e52e1a32e312a2e38e8109244986acb1c40b
sgc93 <smachewgedefaw@gmail.com>
2023-05-13 19:50:05 -0900

    add ScrollReveal effect

--- script.js
+++ script.js
@@ -37,3 +37,19 @@ window.onscroll = () => {
   menuIcon.classList.toggle("x-icon");
   navbar.classList.toggle("active");
 };
+
+// scroll reveal
+ScrollReveal({
+  // reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
